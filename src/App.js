fa0a757894dee8e75bd081d73134924c9823dfec
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2022-11-13 04:36:50 +0200

    created container folder
    added the features, blog, possibility, footer, whatGPT3 and header components. Imported the to the App.js file.
    
    Imported the cta, brand and navbar components to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers/index';
+import { CTA, Brand, Navbar } from './components/index';
 
 function App() {
   return (
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+import { default as Blog } from './blog/Blog';
+import { default as Feature } from './features/Features';
+import { default as Footer } from './footer/Footer';
+import { default as Header } from './header/Header';
+import { default as Possibility  } from './possibility/Possibility';
+import { default as WhatGPT3 } from './whatGpt3/WhatGPT3';  
